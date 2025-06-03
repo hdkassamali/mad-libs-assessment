f4bf6002c7bc6092afdfefdf6de6c1d8e98e2f0c
hdkassamali <122648642+hdkassamali@users.noreply.github.com>
2025-06-02 17:54:35 -0700

    finish part 2 add commas

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,28 @@
-function addCommas() {}
+function addCommas(num) {
+  let numStr = num.toString();
+
+  const isNegative = numStr[0] === "-";
+  if (isNegative) {
+    numStr = numStr.slice(1);
+  }
+
+  const [integerPart, decimalPart] = numStr.split(".");
+
+  let formattedInteger = "";
+  for (let i = 0; i < integerPart.length; i++) {
+    if (i > 0 && (integerPart.length - i) % 3 === 0) {
+      formattedInteger += ",";
+    }
+    formattedInteger += integerPart[i];
+  }
+
+  let result = isNegative ? "-" : "";
+  result += formattedInteger;
+  if (decimalPart) {
+    result += "." + decimalPart;
+  }
+
+  return result;
+}
 
 module.exports = addCommas;
